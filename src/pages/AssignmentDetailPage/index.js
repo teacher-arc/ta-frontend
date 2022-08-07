3a044c9fe525faf06328f39124d7969ccc4cb722
Sheetal Singh <sheetalsinghnew@gmail.com>
2022-08-07 11:07:30 +0530

    netlify settings

--- src/pages/AssignmentDetailPage/index.js
+++ src/pages/AssignmentDetailPage/index.js
@@ -1,7 +1,44 @@
 import React from "react";
-
+import RectangleIllustration from "../../widgets/RectangleIllustration";
+import { Box, Heading, Text, Center } from "@chakra-ui/react";
+function ProgressBar() {
+  return (
+    <Box
+      position="absolute"
+      borderRadius="24px"
+      margin="5%"
+      alignItems="right"
+      height="30%"
+      width="90%"
+      backgroundColor="#FFFFFF"
+    ></Box>
+  );
+}
 function AssignmentDetailsPage() {
-  return <div>AssignmentDetailsPage</div>;
+  return (
+    <div>
+      <Box pos="relative" width="100%" display="flex" zIndex="0">
+        <RectangleIllustration />
+      </Box>
+
+      <Heading as="h4" margin="0 auto" width="60%" paddingBottom="20px">
+        Transaction Details
+      </Heading>
+
+      <Box
+        position="relative"
+        borderRadius="24px"
+        margin="0 auto"
+        alignItems="right"
+        height="70vh"
+        width="60%"
+        backgroundColor="#6B96E1"
+        zindex="5"
+      >
+        <ProgressBar />
+      </Box>
+    </div>
+  );
 }
 
 export default AssignmentDetailsPage;
